7c826125e102a9101ebdc5175fe645c096a03810
widesky0 <kingyan09@qq.com>
2019-11-01 11:40:54 +0800

    '3.框架简介'

---
+++ src/pages/about/about.jsx
@@ -0,0 +1,14 @@
+import Taro,{Component} from "@tarojs/taro"
+import {View,Text} from '@tarojs/components'
+import './about.css'
+
+export default class About extends Component{
+    //初始化配置
+
+    //生命周期
+
+    //render
+    render(){
+        return <div>1234</div>
+    }
+}
